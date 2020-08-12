d67c129b766566015281ea5806b2c15a0e58ee29
VMJWV <649685018@qq.com>
2020-08-12 09:36:01 +0800

    bug fixed

--- src/main/java/top/kingworker/blog/BlogApplication.java
+++ src/main/java/top/kingworker/blog/BlogApplication.java
@@ -3,10 +3,11 @@ package top.kingworker.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //扫描所有的mapper
 @MapperScan("top.kingworker.blog.mapper")
-@EnableTransactionManagement 
+@EnableTransactionManagement
 @SpringBootApplication
 public class BlogApplication {
 
